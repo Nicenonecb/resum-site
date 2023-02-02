2c17ee0147574218f4eb44ee61953289727f78f1
wbz <1664546556@qq.com>
2023-02-02 21:53:50 +0800

    feature: add antd

--- src/components/my-resume/base-info.tsx
+++ src/components/my-resume/base-info.tsx
@@ -1,13 +1,23 @@
+import React from 'react';
+import { Divider,Input } from 'antd';
+
 
 
 
 export default function BaseInfo() {
 
+
   return (
-    <div>
+    <div className=" gap-x-2">
+      <div className=" flex ">
+        <div>2018届毕业</div>
+        <Divider type="vertical" className='bg-white' />
+        <div>前端开发</div>
+      </div>
       <div>邮箱:bmljZW5vbmVjYkBnbWFpbC5jb20= </div>
       <div>微信:TmljZW5vbmVjYg==  </div>
-      <div>2018届毕业</div>
+      
+      <Input placeholder="Basic usage" className='mt-10' />
     </div>
   )
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,11 @@ import '../styles/index.module.css'
 export default function Index() {
   return (
     <section className="h-full w-full bg-gray-700 ">
-        <section className="flex flex-col items-center   h-full ">
-          <div className="mt-10"> 王秉政</div>
-          <div>
-            <BaseInfo></BaseInfo>
-          </div>
-        </section>
+      <section className="flex flex-col items-center   h-full ">
+        <div className="mt-10 text-lg"> 王秉政</div>
+        <BaseInfo></BaseInfo>
+
+      </section>
 
     </section>
   )
